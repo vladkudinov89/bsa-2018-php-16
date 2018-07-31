d26368fbfb72f07464be6086dcab70adc3e72357
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-07-31 14:02:16 +0300

    Add search user

--- src/components/UserList/SearchUser.vue
+++ src/components/UserList/SearchUser.vue
@@ -10,66 +10,88 @@
 
         <div class="">
             <div>Select search by</div>
-            <select class="ui dropdown selection search_select " v-model="searchBy" >
+            <select class="ui dropdown selection search_select " v-model="searchBy">
                 <option v-for="paramSearch in paramsSearch" v-bind:value="paramSearch.nameParam">
                     {{paramSearch.nameParam}}
                 </option>
             </select>
         </div>
-        <button class="ui primary button" v-bind:class="{disabled : btnSearch }" @click="filterUser">Search</button>
+        <button class="ui primary button" @click="filterUser">Search</button>
+
+        <table v-if="usersResultFind.length > 0" class="ui very basic collapsing celled table">
+            <thead>
+            <tr>
+                <th>User</th>
+                <th>Email</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            <tr v-for="userResultFind in usersResultFind">
+                <td>
+                    <h4 class="ui image header">
+                        <img :src="userResultFind.avatar" class="ui mini rounded image">
+                        <div class="content">
+                            {{ userResultFind.name }}
+
+                        </div>
+                    </h4>
+                </td>
+                <td>
+                    {{ userResultFind.email }}
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <div class="no_result" v-else="usersResultFind">
+            <h2>No result</h2>
+        </div>
     </div>
 </template>
 
 <script>
     import {mapState} from 'vuex';
+
     export default {
-        name: "SearchUser",
-        // props : {
-        //     users : Array
-        // },
-        computed: {
-            ...mapState({
-                users: state => state.users.users,
-            }),
-        },
-        data(){
-          return {
-              searchUser: '',
-              searchBy: '',
-              btnSearch: true,
-              paramsSearch: [
-                  {nameParam: 'name', value: 1},
-                  {nameParam: 'email', value: 2}
-              ],
-          }
-        },
-        watch: {
-            searchBy: function () {
-                if (this.searchBy.length > 0) {
-                    this.btnSearch = false;
-                }
+        data() {
+            return {
+                usersResultFind: [],
+                searchUser: '',
+                searchBy: '',
+                paramsSearch: [
+                    {nameParam: 'name', value: 1},
+                    {nameParam: 'email', value: 2}
+                ],
             }
         },
-        methods : {
+        computed: {
+            ...mapState({
+                users: state => state.users.users
+            }),
+        },
+        methods: {
             filterUser() {
                 const searchBy = this.searchBy;
-                console.log(searchBy);
+                // console.log(searchBy);
                 const searchUser = this.searchUser;
                 if (searchBy === 'name') {
-                    this.users = this.users.filter(function (findUser) {
+                    this.usersResultFind = this.users.filter(function (findUser) {
                         return findUser.name.indexOf(searchUser) > -1;
                     });
                 }
                 if (searchBy === 'email') {
-                    this.users = this.users.filter(function (findUser) {
+                    this.usersResultFind = this.users.filter(function (findUser) {
                         return findUser.email.indexOf(searchUser) > -1;
                     });
                 }
+                // console.log(this.usersResultFind);
             },
         }
     }
 </script>
 
 <style scoped>
-
+.no_result{
+    margin: 20px 0;
+}
 </style>
--- src/components/UserList/Users.vue
+++ src/components/UserList/Users.vue
@@ -4,28 +4,28 @@
         <!--<template>-->
             <!--<search-user></search-user>-->
         <!--</template>-->
-        <div class='search_panel'>
-            <h2>Search</h2>
-            <div>
-                <div class="ui left icon input search_input">
-                    <input type="text" placeholder="Search users..." v-model="searchUser">
-                    <i class="users icon"></i>
-                </div>
-            </div>
-            <!--<div class="" v-for="filterItem in filterUser">-->
-                <!--<h3>{{filterItem.name}}</h3>-->
+        <!--<div class='search_panel'>-->
+            <!--<h2>Search</h2>-->
+            <!--<div>-->
+                <!--<div class="ui left icon input search_input">-->
+                    <!--<input type="text" placeholder="Search users..." v-model="searchUser">-->
+                    <!--<i class="users icon"></i>-->
+                <!--</div>-->
             <!--</div>-->
 
-            <div class="">
-                <div>Select search by</div>
-                <select class="ui dropdown selection search_select " v-model="searchBy" >
-                    <option v-for="paramSearch in paramsSearch" v-bind:value="paramSearch.nameParam">
-                        {{paramSearch.nameParam}}
-                    </option>
-                </select>
-            </div>
-            <button class="ui primary button" v-bind:class="{disabled : btnSearch }" @click="filterUser()">Search</button>
-        </div>
+            <!--<div class="">-->
+                <!--<div>Select search by</div>-->
+                <!--<select class="ui dropdown selection search_select " v-model="searchBy" >-->
+                    <!--<option v-for="paramSearch in paramsSearch" v-bind:value="paramSearch.nameParam">-->
+                        <!--{{paramSearch.nameParam}}-->
+                    <!--</option>-->
+                <!--</select>-->
+            <!--</div>-->
+            <!--<button class="ui primary button" v-bind:class="{disabled : btnSearch }" @click="filterUser()">Search</button>-->
+        <!--</div>-->
+        <template>
+            <search-user @clicked="onShowUserList"></search-user>
+        </template>
         <div class="ui cards">
             <template v-for="(user, id) in users">
                 <user-item :key="id" :index="id" :user="user"></user-item>
@@ -36,73 +36,31 @@
 
 <script>
 
-    import {mapState , mapMutations} from 'vuex';
+    import {mapState} from 'vuex';
     import UserItem from './UserItem';
-    // import users from "../../store/modules/users";
-    // import SearchUser from './SearchUser'
+    import SearchUser from './SearchUser'
 
     export default {
         name: "Users",
         components: {
             UserItem,
-            // SearchUser
+            SearchUser
         },
         data(){
             return {
-                searchUser: '',
-                searchBy: '',
-                btnSearch: true,
-                paramsSearch: [
-                    {nameParam: 'name', value: 1},
-                    {nameParam: 'email', value: 2}
-                ],
-            }
-        },
-        watch: {
-            searchBy: function () {
-                if (this.searchBy.length > 0) {
-                    this.btnSearch = false;
-                }
+                showUserList: true
             }
         },
         methods : {
-            filterUser() {
-              this.$store.commit('filterUser' ,this.searchUser);
-            },
-            // ...mapMutations({
-            //     add: 'filterUser' // map `this.add()` to `this.$store.commit('increment')`
-            // }),
-            // ...mapMutations([
-            //    'filterUser'
-            // ]),
-            // filterUser() {
-            //   this.$store.commit('filterUser',this.searchUser);
-            // },
-            // filterUser() {
-            //     const searchBy = this.searchBy;
-            //     // console.log(searchBy);
-            //     const searchUser = this.searchUser;
-            //     // console.log(users.state.users);
-            //     // if (searchBy === 'name') {
-            //     //     users.state.users = users.state.users.filter(function (findUser) {
-            //     //         return findUser.name.indexOf(searchUser) > -1;
-            //     //     });
-            //     // }
-            //     // if (searchBy === 'email') {
-            //     //     users.state.users = users.state.users.filter(function (findUser) {
-            //     //         return findUser.email.indexOf(searchUser) > -1;
-            //     //     });
-            //     // }
-            // },
+            onShowUserList(value) {
+                this.showUserList = value;
+            }
         },
         computed: {
             ...mapState({
                 users: state => state.users.users,
             }),
 
-            // ...mapGetters([
-            //     'filterUser'
-            // ])
         }
     }
 </script>
